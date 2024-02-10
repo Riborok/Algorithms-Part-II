70d363ae67f889d67bd446182cb721a504ce917e
Egor Pankratiev <112758007+Riborok@users.noreply.github.com>
2024-02-10 18:40:03 +0300

    Refactored BaseBall code

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -18,16 +18,18 @@ public class BaseballElimination {
     private int[] losses;
     private int[] remaining;
     private int[][] against;
-    private boolean hasTeam(String team) {
-        return teamIndices.containsKey(team);
+
+    public BaseballElimination(String filename) {
+        In in = new In(filename);
+        readNumberOfTeamsFromFile(in);
+        createTeamStats();
+        readStatsFromFile(in);
     }
-    private void validateTeam(String team) {
-        if (!hasTeam(team))
-            throw new IllegalArgumentException();
-    }
+
     private void readNumberOfTeamsFromFile(In in) {
         numOfTeams = in.readInt();
     }
+
     private void createTeamStats() {
         teams = new String[numOfTeams];
         wins = new int[numOfTeams];
@@ -35,11 +37,13 @@ public class BaseballElimination {
         remaining = new int[numOfTeams];
         against = new int[numOfTeams][numOfTeams];
     }
-    private void readAgainstFromFile(In in, int teamIndex) {
+
+    private void readStatsFromFile(In in) {
         for (int i = 0; i < numOfTeams; i++)
-            against[teamIndex][i] = in.readInt();
+            readTeamStatFromFile(in, i);
     }
-    private void readTeamStatsFromFile(In in, int teamIndex) {
+
+    private void readTeamStatFromFile(In in, int teamIndex) {
         String team = in.readString();
         teamIndices.put(team, teamIndex);
         teams[teamIndex] = team;
@@ -48,41 +52,88 @@ public class BaseballElimination {
         remaining[teamIndex] = in.readInt();
         readAgainstFromFile(in, teamIndex);
     }
-    private void readTeamStatsFromFile(In in) {
+
+    private void readAgainstFromFile(In in, int teamIndex) {
         for (int i = 0; i < numOfTeams; i++)
-            readTeamStatsFromFile(in, i);
+            against[teamIndex][i] = in.readInt();
     }
-    private boolean isAchievableTeamWithoutRemainingGamesPlayedBetweenOthers(int teamIndex, int targetTeam) {
-        return wins[teamIndex] + remaining[teamIndex] >= wins[targetTeam];
+
+    public int numberOfTeams() {
+        return numOfTeams;
     }
+
+    public Iterable<String> teams() {
+        return List.of(teams);
+    }
+
+    public int wins(String team) {
+        validateTeam(team);
+        return wins[teamIndices.get(team)];
+    }
+
+    public int losses(String team) {
+        validateTeam(team);
+        return losses[teamIndices.get(team)];
+    }
+
+    public int remaining(String team) {
+        validateTeam(team);
+        return remaining[teamIndices.get(team)];
+    }
+
+    public int against(String team1, String team2) {
+        validateTeam(team1);
+        validateTeam(team2);
+        return against[teamIndices.get(team1)][teamIndices.get(team2)];
+    }
+
+    public boolean isEliminated(String team) {
+        validateTeam(team);
+        var certificate = findEliminationCertificate(teamIndices.get(team));
+        return !certificate.isEmpty();
+    }
+
+    public Iterable<String> certificateOfElimination(String team) {
+        validateTeam(team);
+        var certificate = findEliminationCertificate(teamIndices.get(team));
+        return certificate.isEmpty() ? null : certificate;
+    }
+
+    private void validateTeam(String team) {
+        if (!hasTeam(team))
+            throw new IllegalArgumentException();
+    }
+
+    private boolean hasTeam(String team) {
+        return teamIndices.containsKey(team);
+    }
+
+    private List<String> findEliminationCertificate(int teamIndex) {
+        List<String> certificate = new ArrayList<>();
+        if (isTeamEliminatedWithoutRemainingGamesPlayedBetweenOthers(teamIndex, certificate))
+            return certificate;
+
+        var flowNetwork = new FlowNetwork(calcNumOfVertices());
+
+        addGameVertices(flowNetwork, teamIndex);
+        addTeamVertices(flowNetwork, teamIndex);
+
+        findEliminationCertificate(flowNetwork, teamIndex, certificate);
+        return certificate;
+    }
+
     private boolean isTeamEliminatedWithoutRemainingGamesPlayedBetweenOthers(int teamIndex, List<String> certificate) {
         for (int i = 0; i < numOfTeams; i++)
             if (!isAchievableTeamWithoutRemainingGamesPlayedBetweenOthers(teamIndex, i))
                 certificate.add(teams[i]);
         return !certificate.isEmpty();
     }
-    private boolean isNotCheckableTeam(int teamIndex, int otherTeamIndex) {
-        return teamIndex != otherTeamIndex;
+
+    private boolean isAchievableTeamWithoutRemainingGamesPlayedBetweenOthers(int teamIndex, int targetTeam) {
+        return wins[teamIndex] + remaining[teamIndex] >= wins[targetTeam];
     }
-    private int calcNumOfTeamVertices() {
-        return numOfTeams - 1;
-    }
-    private int calcNumOfGameVertices() {
-        return numOfTeams * (numOfTeams - 1) / 2;
-    }
-    private int calcNumOfVertices() {
-        return 2 + calcNumOfGameVertices() + calcNumOfTeamVertices();
-    }
-    private int getSourceVertex() {
-        return 0;
-    }
-    private int calcTargetVertex() {
-        return calcNumOfVertices() - 1;
-    }
-    private int calcTeamVertex(int numOfGameVertices, int teamIndex) {
-        return numOfGameVertices + teamIndex;
-    }
-    private void addGameVerticesToFlowNetwork(FlowNetwork flowNetwork, int teamIndex) {
+
+    private void addGameVertices(FlowNetwork flowNetwork, int teamIndex) {
         int numOfGameVertices = calcNumOfGameVertices();
         int sourceVertex = getSourceVertex();
         int gameVertex = sourceVertex + 1;
@@ -91,32 +142,44 @@ public class BaseballElimination {
             if (isNotCheckableTeam(teamIndex, i)) {
                 for (int j = i + 1; j < numOfTeams; j++) {
                     if (isNotCheckableTeam(teamIndex, j)) {
-                        flowNetwork.addEdge(new FlowEdge(sourceVertex, gameVertex, against[i][j]));
-                        flowNetwork.addEdge(new FlowEdge(gameVertex, calcTeamVertex(numOfGameVertices, i), Double.POSITIVE_INFINITY));
-                        flowNetwork.addEdge(new FlowEdge(gameVertex, calcTeamVertex(numOfGameVertices, j), Double.POSITIVE_INFINITY));
+                        int teamVertex1 = calcTeamVertex(numOfGameVertices, i);
+                        int teamVertex2 = calcTeamVertex(numOfGameVertices, j);
+                        addGameEdges(flowNetwork, sourceVertex, gameVertex, teamVertex1, teamVertex2, against[i][j]);
                         gameVertex++;
                     }
                 }
             }
         }
     }
-    private void addTeamVerticesToFlowNetwork(FlowNetwork flowNetwork, int teamIndex) {
+
+    private void addGameEdges(FlowNetwork flowNetwork, int sourceVertex, int gameVertex,
+                              int teamVertex1, int teamVertex2, int againstValue) {
+        flowNetwork.addEdge(new FlowEdge(sourceVertex, gameVertex, againstValue));
+        flowNetwork.addEdge(new FlowEdge(gameVertex, teamVertex1, Double.POSITIVE_INFINITY));
+        flowNetwork.addEdge(new FlowEdge(gameVertex, teamVertex2, Double.POSITIVE_INFINITY));
+    }
+
+    private void addTeamVertices(FlowNetwork flowNetwork, int teamIndex) {
         int numOfGameVertices = calcNumOfGameVertices();
         int targetVertex = calcTargetVertex();
 
-        for (int i = 0; i < numOfTeams; i++) {
+        for (int i = 0; i < numOfTeams; i++)
             if (isNotCheckableTeam(teamIndex, i)) {
-                int capacity = wins[teamIndex] + remaining[teamIndex] - wins[i];
-                flowNetwork.addEdge(new FlowEdge(calcTeamVertex(numOfGameVertices, i), targetVertex, capacity));
+                int teamVertex = calcTeamVertex(numOfGameVertices, i);
+                int capacity = calcCapacity(teamIndex, i);
+                addTeamEdge(flowNetwork, teamVertex, targetVertex, capacity);
             }
-        }
     }
-    private boolean isEliminatedTeamInCut(int teamIndex, int checkedTeamIndex,
-                                          FordFulkerson fordFulkerson, int numOfGameVertices) {
-        return isNotCheckableTeam(teamIndex, checkedTeamIndex) &&
-                fordFulkerson.inCut(calcTeamVertex(numOfGameVertices, checkedTeamIndex));
+
+    private int calcCapacity(int teamIndex, int otherTeamIndex) {
+        return wins[teamIndex] + remaining[teamIndex] - wins[otherTeamIndex];
     }
-    private void findEliminationCertificateInFlowNetwork(FlowNetwork flowNetwork, int teamIndex, List<String> certificate) {
+
+    private void addTeamEdge(FlowNetwork flowNetwork, int teamVertex, int targetVertex, int capacity) {
+        flowNetwork.addEdge(new FlowEdge(teamVertex, targetVertex, capacity));
+    }
+
+    private void findEliminationCertificate(FlowNetwork flowNetwork, int teamIndex, List<String> certificate) {
         int numOfGameVertices = calcNumOfGameVertices();
         FordFulkerson fordFulkerson = new FordFulkerson(flowNetwork, getSourceVertex(), calcTargetVertex());
 
@@ -124,58 +187,41 @@ public class BaseballElimination {
             if (isEliminatedTeamInCut(teamIndex, i, fordFulkerson, numOfGameVertices))
                 certificate.add(teams[i]);
     }
-    private List<String> findEliminationCertificate(int teamIndex) {
-        List<String> certificate = new ArrayList<>();
-        if (isTeamEliminatedWithoutRemainingGamesPlayedBetweenOthers(teamIndex, certificate))
-            return certificate;
 
-        var flowNetwork = new FlowNetwork(calcNumOfVertices());
+    private boolean isEliminatedTeamInCut(int teamIndex, int checkedTeamIndex,
+                                          FordFulkerson fordFulkerson, int numOfGameVertices) {
+        return isNotCheckableTeam(teamIndex, checkedTeamIndex) &&
+                fordFulkerson.inCut(calcTeamVertex(numOfGameVertices, checkedTeamIndex));
+    }
 
-        addGameVerticesToFlowNetwork(flowNetwork, teamIndex);
-        addTeamVerticesToFlowNetwork(flowNetwork, teamIndex);
+    private int calcTeamVertex(int numOfGameVertices, int teamIndex) {
+        return numOfGameVertices + teamIndex;
+    }
 
-        findEliminationCertificateInFlowNetwork(flowNetwork, teamIndex, certificate);
-        return certificate;
+    private boolean isNotCheckableTeam(int teamIndex, int otherTeamIndex) {
+        return teamIndex != otherTeamIndex;
     }
-    public BaseballElimination(String filename) {
-        In in = new In(filename);
-        readNumberOfTeamsFromFile(in);
-        createTeamStats();
-        readTeamStatsFromFile(in);
+
+    private int getSourceVertex() {
+        return 0;
     }
-    public int numberOfTeams() {
-        return numOfTeams;
+
+    private int calcTargetVertex() {
+        return calcNumOfVertices() - 1;
     }
-    public Iterable<String> teams() {
-        return List.of(teams);
+
+    private int calcNumOfVertices() {
+        return 2 + calcNumOfGameVertices() + calcNumOfTeamVertices();
     }
-    public int wins(String team) {
-        validateTeam(team);
-        return wins[teamIndices.get(team)];
+
+    private int calcNumOfTeamVertices() {
+        return numOfTeams - 1;
     }
-    public int losses(String team) {
-        validateTeam(team);
-        return losses[teamIndices.get(team)];
+
+    private int calcNumOfGameVertices() {
+        return numOfTeams * (numOfTeams - 1) / 2;
     }
-    public int remaining(String team) {
-        validateTeam(team);
-        return remaining[teamIndices.get(team)];
-    }
-    public int against(String team1, String team2) {
-        validateTeam(team1);
-        validateTeam(team2);
-        return against[teamIndices.get(team1)][teamIndices.get(team2)];
-    }
-    public boolean isEliminated(String team) {
-        validateTeam(team);
-        var certificate = findEliminationCertificate(teamIndices.get(team));
-        return !certificate.isEmpty();
-    }
-    public Iterable<String> certificateOfElimination(String team) {
-        validateTeam(team);
-        var certificate = findEliminationCertificate(teamIndices.get(team));
-        return certificate.isEmpty() ? null : certificate;
-    }
+
     public static void main(String[] args) {
         BaseballElimination division = new BaseballElimination(args[0]);
         for (String team : division.teams()) {
